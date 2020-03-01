3796a6f42129819240af8da89a95037af3829a4d
MidNightBlue292 <manuelpereira292@gmail.com>
2020-03-01 22:22:50 +0000

    016 Quebrando um número

---
+++ exer_016.py
@@ -0,0 +1,6 @@
+from math import trunc
+nr = float(input('Digite um número real -> '))
+# Modo 1
+print('A sua porcão inteira é {}'.format(trunc(nr)))
+# Modo 2
+print('A sua porção inteira é {}'.format(int(nr)))
