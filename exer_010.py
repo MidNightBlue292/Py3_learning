7d86ade4f773c560e470f9257ea4833e2c5d75b4
MidNightBlue292 <manuelpereira292@gmail.com>
2020-03-01 22:10:51 +0000

    010 Conversor de moedas

---
+++ exer_010.py
@@ -0,0 +1,4 @@
+euro = float(input('Quanto dinheiro tens na carteira? '))
+dolar = euro * 1.11
+print('Com {:.2f}€ consegues comprar {:.2f}$'.format(euro, dolar))
+print('Taxa de conversão em 2019/11/15\n1€ == 1.11$')
