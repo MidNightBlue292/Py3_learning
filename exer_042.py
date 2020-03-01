f379be8956658c84a7e6882d1aa9e6f7efe81a2e
MidNightBlue292 <manuelpereira292@gmail.com>
2020-03-01 23:00:29 +0000

    042 Analisando triângulos v2

---
+++ exer_042.py
@@ -0,0 +1,13 @@
+r1=float(input('Segmento de recta 1 -> '))
+r2=float(input('Segmento de recta 2 -> '))
+r3=float(input('Segemnto de recta 3 -> '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos de recta podem formar um triângulo')
+    if r1 == r2 == r3:
+        print('EQUILÁTERO')
+    elif r1 != r2 != r3 != r1:
+        print('ESCALENO')
+    else:
+        print('ISÓSCELES')
+else:
+    print('Os segmentos de recta não podem formar um triângulo')
