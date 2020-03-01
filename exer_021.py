b2bdc53541bc9298f58d29d0ba1ae635f6749e43
MidNightBlue292 <manuelpereira292@gmail.com>
2020-03-01 22:30:41 +0000

    021 Tocando um MP3

---
+++ exer_021.py
@@ -0,0 +1,39 @@
+import pygame
+
+def pmusic(file):
+    pygame.init()
+    pygame.mixer.init()
+    clock = pygame.time.Clock()
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play()
+    print("Playing...")
+    while pygame.mixer.music.get_busy():
+        clock.tick(1000)
+
+
+def stopmusic():
+    pygame.mixer.music.stop()
+
+
+def getmixerargs():
+    pygame.mixer.init()
+    freq, size, chan = pygame.mixer.get_init()
+    return freq, size, chan
+
+
+def initMixer():
+    BUFFER = 3072  # audio buffer size, number of samples since pygame 1.8.
+    FREQ, SIZE, CHAN = getmixerargs()
+    pygame.mixer.init(FREQ, SIZE, CHAN, BUFFER)
+
+
+try:
+    initMixer()
+    file = 'exer021.mp3'
+    pmusic(file)
+except KeyboardInterrupt:  # to stop playing, press "ctrl-c"
+    stopmusic()
+    print("\nPlay Stopped by user")
+except Exception:
+    print("unknown error")
+print("Done")
